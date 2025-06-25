69e761bffbc930caf8273193fa680caf00ff2f49
Leandro Lourenci <leandro@lourenci.com>
2025-06-25 11:34:31 +1000

    Concurrently fetch all user repositories

--- usecases/get_all_user_repos.go
+++ usecases/get_all_user_repos.go
@@ -3,18 +3,20 @@ package usecases
 import "github.com/lourenci/github-alfred/lib/github"
 
 func GetAllUserRepos(githubRepository github.GitHub) []github.Repository {
-	var repos []github.Repository
+	channels := make(chan []github.Repository)
+	func(c chan []github.Repository) {
+		go func() {
+			c <- githubRepository.StarredRepos()
+		}()
+		go func() {
+			c <- githubRepository.UserRepos()
+		}()
+		go func() {
+			c <- githubRepository.WatchedRepos()
+		}()
+	}(channels)
 
-	repos = append(
-		append(
-			append(
-				repos,
-				githubRepository.StarredRepos()...,
-			),
-			githubRepository.UserRepos()...,
-		),
-		githubRepository.WatchedRepos()...,
-	)
+	repos := append(append(<-channels, <-channels...), <-channels...)
 
 	return repos
 }
