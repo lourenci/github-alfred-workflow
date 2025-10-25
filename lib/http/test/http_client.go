f70e544571ec12f0ae9829c367305db82ef77f12
Leandro Lourenci <leandro@lourenci.com>
2025-10-25 15:51:29 +1100

    Fix race condition in fake HTTP client

--- lib/http/test/http_client.go
+++ lib/http/test/http_client.go
@@ -3,6 +3,7 @@ package test
 import (
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 type Call struct {
@@ -10,6 +11,7 @@ type Call struct {
 }
 
 type fakeHttpClient struct {
+	lock     sync.Mutex
 	Calls    map[url.URL][]Call
 	requests map[url.URL][]http.Response
 }
@@ -22,6 +24,8 @@ func NewFakeHttpClient(requests map[url.URL][]http.Response) *fakeHttpClient {
 }
 
 func (f *fakeHttpClient) Get(url url.URL, headers map[string]string) (*http.Response, error) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
 	f.Calls[url] = append(f.Calls[url], Call{Headers: headers})
 
 	return &f.requests[url][len(f.Calls[url])-1], nil
